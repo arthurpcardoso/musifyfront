1a99e7f7b934983008b4f778fc829846e0537ef7
arthurpcardoso <tuco1301@gmail.com>
2023-02-07 07:46:49 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // in src/App.tsx
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
-    <Resource name="comments" list={ListGuesser} />
+    <Resource name="users" list={ListGuesser} edit={EditGuesser} />
+    <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
   </Admin>
 );
 
